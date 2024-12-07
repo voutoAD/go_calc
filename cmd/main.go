006355b8df8a0a47fb0349c9db32441329cd608e
alex <massalex0209@gmail.com>
2024-12-07 14:26:25 +0300

    added main and app packages

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"github.com/voutoad/go_calc/internal/application"
+)
+
+func main() {
+	app := application.NewApplication()
+	app.Run()
+}
--- internal/application/application.go
+++ internal/application/application.go
@@ -1,1 +1,38 @@
 package application
+
+import (
+	"bufio"
+	calc "github.com/voutoad/go_calc/pkg/go_calc"
+	"log"
+	"os"
+	"strings"
+)
+
+type Application struct {
+}
+
+func NewApplication() *Application {
+	return &Application{}
+}
+
+func (app *Application) Run() error {
+	for {
+		log.Println("input expression")
+		reader := bufio.NewReader(os.Stdin)
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("failed to read from console")
+		}
+		text = strings.TrimSpace(text)
+		if text == "exit" {
+			log.Println("application stopped")
+			return nil
+		}
+		result, err := calc.Calc(text)
+		if err != nil {
+			log.Printf("%s calculation failed with error: %s\n", text, err)
+		} else {
+			log.Printf("%s=%f\n", text, result)
+		}
+	}
+}
